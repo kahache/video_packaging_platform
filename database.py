136c7c92f5861f1330bf55c920167b1f2f3192af
Javier Brines <javierbrines@FIGURIN.local>
2020-07-25 16:35:55 +0200

    Database creation & class enabling for operations

---
+++ database.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+class Database:
+
+    def __init__():
+        conn=sqlite3.connect("movies.db")
+        cur=conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS movie_files (input_content TEXT, input_content_id INTEGER, file_path TEXT, key TEXT, kid TEXT, packaged_content_id TEXT, url TEXT)")
+        conn.commit()
+        conn.close
+
+
+    def insert(input_content,input_content_id,file_path,key,kid,packaged_content_id,url):
+        conn=sqlite3.connect("movies.db")
+        cur=conn.cursor()
+        cur.execute("INSERT INTO movie_files VALUES (?,?,?,?,?,?,?)",(input_content,input_content_id,file_path,key,kid,packaged_content_id,url))
+        conn.commit()
+        conn.close
+
+    def view():
+        conn=sqlite3.connect("movies.db")
+        cur=conn.cursor()
+        cur.execute("SELECT * FROM movie_files")
+        rows=cur.fetchall()
+        conn.close()
+        return rows
+
+    def delete(input_content):
+        conn=sqlite3.connect("movies.db")
+        cur=conn.cursor()
+        cur.execute("DELETE FROM movie_files WHERE input_content=?",(input_content,))
+        conn.commit()
+        conn.close()
+
+    def update(input_content_id,file_path,input_content):
+        conn=sqlite3.connect("movies.db")
+        cur=conn.cursor()
+        cur.execute("UPDATE movie_files SET input_content_id=?, file_path=? WHERE uploaded_file=?",(input_content_id,file_path,input_content))
+        conn.commit()
+        conn.close()
+
+    def update_prueba(tipo,valor):
+        conn=sqlite3.connect("movies.db")
+        cur=conn.cursor()
+        cur.execute("UPDATE movie_files SET tipo=valor WHERE uploaded_file=?",(tipo))
+        conn.commit()
+        conn.close()
+
+#print(view())
+#create_table()
+#insert("archivo_1",2,"/archivo/dondesencuentra","NULL","NULL","NULL","NULL")
+#connect()
+Database.__init__()
+print(Database.view())
