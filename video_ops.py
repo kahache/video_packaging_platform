fdc294852986a62526f54393ccc35e3819538c0d
Javier Brines <javierbrines@FIGURIN.local>
2020-07-27 11:16:44 +0200

    Video Operations script!

--- video_ops.py
+++ video_ops.py
@@ -5,6 +5,8 @@ import json
 #import mp4utils
 from database import *
 import ntpath
+import string
+import secrets
 
 #def upload():
     #    subprocess.call('mv '+file_name+' storage/',shell=True)
@@ -38,9 +40,10 @@ def video_ingestion(input_file):
                  shell=True)
                 file_name=ntpath.basename(input_file)
                 Database.insert(input_file,None,(storage_dir+"/"+file_name),
-                video_track,"Ingested",None,None,None,None)
+                video_track,"Ingested",None,None,None,None,None)
             else:
                 print("Error: file {} doesn't contain a video track".format(input_file))
+                #turn this into return
                 #OPTIONAL - we erase the input_file
                 #subprocess.run("rm {}".format(input_file),
                 #stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
@@ -60,11 +63,29 @@ def video_ingestion(input_file):
     #video_track = "2"
 
 
-def fragment(input_file,output_file,otra_cosa):
-    #fragment_custom_command = ("./mp4fragment " + input_file + " " + output_file)
-    Database.update('status','Fragmented','1')
+def fragment(row_name,input_content_id):
+    #AÑADIR CHECK DE QUE NO ESTÁ FRAGMENTADO + IF/ELSE
+    file_for_fragment=(Database.view_one_row(row_name,input_content_id))
+    #print(file_for_fragment)
+#    prueba=ntpath.normpath(file_for_fragment)
+#    print(prueba)
+    output_file="/Users/javierbrines/Downloads/prueba_fragmentado.mp4"
+    output_code=''.join(secrets.choice(string.ascii_uppercase +
+     string.digits) for _ in range(6))
+    output_file_path=output_dir+"/"+output_code+"/"+output_code+".mp4"
+    os.chdir(output_dir)
+    os.mkdir(output_code, mode=0o0755)
+    os.chdir(bin_dir)
+    fragment_custom_command = ("./mp4fragment " + str(file_for_fragment) + " " + output_file_path)
+    print(fragment_custom_command)
+    subprocess.run(fragment_custom_command,
+    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+    os.chdir(working_dir)
+    Database.update('status','Fragmented',str(input_content_id))
+    #Database.update('output_file_path',str(output_file_path),str(input_content_id))
 
 def encrypt(input_file,output_file):
+    fragment_custom_command = ("./mp4fragment " + input_file + " " + output_file)
     encrypt_custom_command = ("./bin/mp4encrypt --method MPEG-CBCS --key "+
      str(video_track) +":random:random "+ input_file + " " + output_file)
     #print(fragment_custom_command)
@@ -72,15 +93,16 @@ def encrypt(input_file,output_file):
     Database.update('status','Encrypted','1')
 
 Database.__init__()
-print(Database.view())
+#print(Database.view())
 #video_track_number()
 working_dir = os.getcwd()
 bin_dir=os.getcwd()+"/bin"
 storage_dir=os.getcwd()+"/storage"
 file="/Users/javierbrines/Documents/Rakuten/video_packaging_platform/BigBuckBunny.mp4"
-video_ingestion(file)
-print(Database.view())
-#fragment("archivo","cosa","otra_cosa")
+output_dir=os.getcwd()+"/output"
+#video_ingestion(file)
+#print(Database.view())
+fragment("file_path",15)
 #encrypt()
 
 
