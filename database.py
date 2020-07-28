b60232be4baee3ca1fd0719bae62347cdef3c16e
Javier Brines <javierbrines@FIGURIN.local>
2020-07-28 22:12:23 +0200

    Database functions script (Mysql)

--- database.py
+++ database.py
@@ -1,78 +1,88 @@
-import sqlite3
+import mysql.connector
 
 class Database:
 
-    def __init__():
-        conn=sqlite3.connect("movies.db")
-        cur=conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS movie_files (input_content TEXT, input_content_id INTEGER PRIMARY KEY AUTOINCREMENT, origin_file_path TEXT, video_track_number INTEGER, status TEXT, output_file_path TEXT, key TEXT, kid TEXT, packaged_content_id TEXT, url TEXT)")
-        conn.commit()
-        conn.close
+    def __init__(db_name,table_name):
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(db_name))
+        cursor.execute("CREATE TABLE IF NOT EXISTS {}".format(table_name)+
+        " (input_content_origin VARCHAR(255),input_content_id INT PRIMARY KEY AUTO_INCREMENT,video_track_number INT,status VARCHAR(20),output_file_path VARCHAR(255),video_key TEXT,kid TEXT,packaged_content_id INT UNIQUE,url VARCHAR(255))")
+        connection.close()
+
+    def insert_all(table_name,input_content_origin):
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        #cursor.execute("INSERT INTO movie_files VALUES ({}".format(input_content_origin)+", {}".format(video_track_number)+", {}".format(status)+", {}".format(output_file_path)+", {}".format(video_key)+", {}".format(kid)+", {}".format(packaged_content_id)+", {}".format(url)+")")
+        print("INSERT INTO {}"+format(table_name)+"("+input_content_origin+") VALUES ({}".format(input_content_origin)+")")
+        cursor.execute("INSERT INTO movie_files (input_content_origin) VALUES ('{}'".format(input_content_origin)+")")
+        connection.commit()
+        connection.close()
+
+    def insert_one_row(table_name,row_name,value):
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO {}".format(table_name)+" ("+row_name+") VALUES ('{}'".format(value)+")")
+        connection.commit()
+        connection.close()
+
+    def view_all(table,config):
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM {}".format(table))
+        results=cursor.fetchall()
+        connection.close()
+        return results
 
 
-    def insert(input_content,input_content_id,origin_file_path,
-    video_track_number,status,output_file_path,key,kid,packaged_content_id,url):
-        conn=sqlite3.connect("movies.db")
-        cur=conn.cursor()
-        cur.execute("INSERT INTO movie_files VALUES (?,?,?,?,?,?,?,?,?,?,?)",
-        (input_content,input_content_id,origin_file_path,video_track_number,
-        status,output_file_path,key,kid,packaged_content_id,url))
-        conn.commit()
-        conn.close
-
-    def view_all():
-        conn=sqlite3.connect("movies.db")
-        cur=conn.cursor()
-        cur.execute("SELECT * FROM movie_files")
-        rows=cur.fetchall()
-        conn.close()
-        return rows
-
-
-    def view_one_row(row,input_content_id):
-        conn=sqlite3.connect("movies.db")
-        cur=conn.cursor()
-        cur.execute("SELECT {}".format(row)+
-        " FROM movie_files WHERE input_content_id = {}"
+    def view_one_row(row,table_name,input_content_id):
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        cursor.execute("SELECT {}".format(row)+
+        " FROM {}".format(table)+" WHERE input_content_id = {}"
         .format(input_content_id))
-        #print(row)
-        #print(input_content_id)
-        rows=cur.fetchone()[0]
-        conn.close()
-        return rows
+        row=cursor.fetchone()[0]
+        connection.close()
+        return row
 
-    def delete(input_content):
-        conn=sqlite3.connect("movies.db")
-        cur=conn.cursor()
-        cur.execute("DELETE FROM movie_files WHERE input_content=?",
-        (input_content,))
-        conn.commit()
-        conn.close()
+    def delete_row(table_name,input_content_id):
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        #https://youtu.be/jsuerKRsEyA
+        cursor.execute("DELETE FROM {}".format(table_name)+
+        " WHERE input_content_id = {}".format(input_content_id))
+        connection.commit()
+        connection.close()
 
-    def update(column,value,input_content_id):
-        conn=sqlite3.connect("movies.db")
-        cur=conn.cursor()
-        cur.execute("UPDATE movie_files SET "+column+" = \""+value+
-         "\" WHERE input_content_id = "+input_content_id)
-        conn.commit()
-        conn.close()
+    def update(table_name,column_name,value,input_content_id):
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        cursor.execute("UPDATE "+table_name+" SET "+column_name+" = \""+value+
+        "\" WHERE input_content_id = "+input_content_id)
+        connection.commit()
+        connection.close()
 
-    def update_prueba(column,value,input_content_id):
-        conn=sqlite3.connect("movies.db")
-        cur=conn.cursor()
-        #string_query=("UPDATE movie_files SET "+tipo+" = \""+valor+ "\" WHERE input_content_id = "+input_content_id)
-        #cur.execute("UPDATE movie_files SET %s = %s WHERE input_content_id = %d",(tipo,valor),input_content_id)
-        print(string_query)
-        cur.execute("UPDATE movie_files SET "+column+" = \""+value+
-         "\" WHERE input_content_id = "+input_content_id)
-        #cur.execute("SELECT %s FROM Data where %s=?" % (column, goal), (constrain,))
-        #cur.execute("SELECT "+column+" FROM Data where "+goal+"=?", (constrain,))
-        conn.commit()
-        conn.close()
+#We define the table we're going to use
+db_name="video_files"
+table_name="movie_files"
+#We modify here the MySQL config parameters
+config = {
+    'user': 'root',
+    'password': 'root',
+    'host': '127.0.0.1',
+    'port': '3306',
+    'database': 'video_files'
+}
+Database.__init__(db_name,table_name)
 
-#print(view())
-#create_table()
-#insert("archivo_1",2,"/archivo/dondesencuentra","NULL","NULL","NULL","NULL")
-#connect()
-#Database.__init__()
-#print(Database.view_prueba("file_path",15))
+#Testing purposes
+#Database.insert_new("SupuTamadre.mp4",2,"Follao","ruta/salida.mpd","doojsdfb","obsuadboasbd","None","http://alamierda.com")
+#file="Suputamadre5.mp4"
+#column_name="status"
+#row_name="input_content_origin"
+#value="Fragmented"
+#input_content_id=4
+#Database.insert_one_row(table_name,row_name,file)
+#Database.delete_row(table_name,input_content_id)
+#print(Database.view_all("movie_files",config))
+#Database.update(table_name,column_name,value,str(input_content_id))
