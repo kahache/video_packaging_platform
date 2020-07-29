882a1f59c49a930f7cfd01eb8842c659b6a591e3
Javier Brines <javierbrines@FIGURIN.local>
2020-07-29 18:52:25 +0200

    Video Operations script!

--- app/video_ops.py
+++ app/video_ops.py
@@ -2,47 +2,63 @@ import os
 import subprocess
 import sys
 import json
-#import mp4utils
 from database import *
 import ntpath
 import string
 import secrets
 
-#def upload():
-    #    subprocess.call('mv '+file_name+' storage/',shell=True)
-        #subprocess.run('./bin/mp4info storage/BigBuckBunny.mp4 --format json| grep Video -B1 | cut -d ":" -f 2 | cut -d "," -f 1 | head -1',shell=True)
-        #print(video_track)
-        #order = "'./bin/mp4info storage/BigBuckBunny.mp4 | grep Video -B1 | head -1 | sed 's/[^0-9]*//g'""
-#        video_track = subprocess.run('./bin/mp4info storage/BigBuckBunny.mp4 | grep Video -B1 | head -1 | sed 's/[^0-9]*//g',shell=True)
-#    subprocess.run('./bin/mp4info storage/BigBuckBunny.mp4 --format json| grep Video -B1 | cut -d ":" -f 2 | cut -d "," -f 1 | head -1 >> buffer.txt',shell=True)
-#mp4utils.Bento4Command.options.exec_dir = "/Users/javierbrines/Documents/Rakuten/video_packaging_platform"
-#exec_dir = "/Users/javierbrines/Documents/Rakuten/video_packaging_platform/"
-#result = mp4utils.Mp4Info('--format json',"BigBuckBunny.mp4","/Users/javierbrines/Documents/Rakuten/video_packaging_platform")
-#mp4utils.Mp4Info('',"/Users/javierbrines/Documents/Rakuten/video_packaging_platform/BigBuckBunny.mp4")
-
+#further this exercise, we should consider a file normalisation function
 def video_ingestion(input_file):
-    #input_file = "/Users/javierbrines/Documents/Rakuten/video_packaging_platform/BigBuckBunny.mp4"
     print(input_file)
     os.chdir(bin_dir)
-    subprocess.run("./mp4info {} --format json > out.json".format(input_file),
-    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+    try:
+        subprocess.check_output("./mp4info {} --format json > out.json".format(input_file), shell=True)
+    #    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+        print("\nOK - File"+input_file+" is a video file and is going to be processed!")
+    except subprocess.CalledProcessError as e:
+        print("\nERROR - Corrupted or wrong file, please review the file. Details are:")
+        text_output=("An error has been occured"+'\n'+'\n', e)
+        raise
+        return text_output
     with open('out.json') as f:
         data = json.load(f)
         items = data.get('tracks')
+        video_found_flag=0
         for item in items:
             if item.get('type') == 'Video':
+                video_found_flag=1
                 video_track = (item.get('id'))
-    #            print(video_track)
-                #print(type(video_track))
                 os.chdir(working_dir)
-                subprocess.run("mv {} storage/".format(input_file),
-                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
-                 shell=True)
-                file_name=ntpath.basename(input_file)
-                Database.insert(input_file,None,(storage_dir+"/"+file_name),
-                video_track,"Ingested",None,None,None,None,None)
-            else:
-                print("Error: file {} doesn't contain a video track".format(input_file))
+                try:
+                    subprocess.check_output("mv {}".format(input_file)+" {}".format(storage_dir),
+                    shell=True)
+                    file_name=ntpath.basename(input_file)
+                    text_output=("OK - File "+file+" has been processed and moved to storage")
+                    return text_output
+                except subprocess.CalledProcessError as e:
+                    print("\nERROR - can't move the file to storage\n\n",e)
+                    text_output=("ERROR - can't move the file to storage",e)
+                    return text_output
+                    raise
+        if video_found_flag == 0:
+            print("ERROR - File doesn't contain a video track")
+            text_output=("ERROR - An error has been occured, file doesn't contain an audio track ")
+            return text_output
+#
+#                print(text_output)
+#                return text_output
+#                break
+#            else:
+#                text_output="ERROR - Video file doesn't contain a valid video track"
+#                print(text_output)
+#                return text_output
+#                break
+#        print(video_found_flag)
+#        print(video_track)
+            #    Database.insert(input_file,None,(storage_dir+"/"+file_name),
+            #    video_track,"Ingested",None,None,None,None,None)
+    #        else:
+    #            print("Error: file {} doesn't contain a video track".format(input_file))
                 #turn this into return
                 #OPTIONAL - we erase the input_file
                 #subprocess.run("rm {}".format(input_file),
@@ -50,21 +66,9 @@ def video_ingestion(input_file):
                 # shell=True)
 
 
-
-            #    Database.insert("BigBuckBunny.mp4",1,"/Users/javierbrines/Documents/Rakuten/video_packaging_platform/BigBuckBunny.mp4",video_track,"Ingested",'NULL','NULL','NULL','NULL')
-            #else:
-
-#def encrypt():
-#    video_track_number()
-#    input_file = "/Users/javierbrines/Documents/Rakuten/video_packaging_platform/fragmentao.mp4"
-#    output_file = "/Users/javierbrines/Downloads/prueba_encripted_script.mp4"
-    #key = "hyN9IKGfWKdAwFaE5pm0qg"
-    #kid = "oW5AK5BW43HzbTSKpiu3SQ"
-    #video_track = "2"
-
-
 def fragment(row_name,input_content_id):
     #AÑADIR CHECK DE QUE NO ESTÁ FRAGMENTADO + IF/ELSE
+    #PENSAR QUE PASA SI YA ESTÁ FRAGMENTADO (evitar duplicados)
     file_for_fragment=(Database.view_one_row(row_name,input_content_id))
     #print(file_for_fragment)
 #    prueba=ntpath.normpath(file_for_fragment)
@@ -82,59 +86,34 @@ def fragment(row_name,input_content_id):
     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
     os.chdir(working_dir)
     Database.update('status','Fragmented',str(input_content_id))
-    #Database.update('output_file_path',str(output_file_path),str(input_content_id))
+    Database.update('output_file_path',str(output_file_path),str(input_content_id))
 
-def encrypt(input_file,output_file):
-    fragment_custom_command = ("./mp4fragment " + input_file + " " + output_file)
-    encrypt_custom_command = ("./bin/mp4encrypt --method MPEG-CBCS --key "+
-     str(video_track) +":random:random "+ input_file + " " + output_file)
-    #print(fragment_custom_command)
-    #subprocess.run(encrypt_custom_command,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-    Database.update('status','Encrypted','1')
+def encrypt(row_name,key,kid,input_content_id):
+    file_to_encrypt=(Database.view_one_row(row_name,input_content_id))
+    video_track=(Database.view_one_row("video_track_number",input_content_id))
+    output_file=(Database.view_one_row("output_file_path",input_content_id))
+    os.chdir(bin_dir)
+    encrypt_custom_command = ("./mp4encrypt --method MPEG-CBCS --key " + str(video_track) + ":" + key + ":random " + "--property " + str(video_track) + ":KID:" + kid + " " + file_to_encrypt +  " " + output_file)
+    print(encrypt_custom_command)
+    subprocess.run(encrypt_custom_command,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+    os.chdir(working_dir)
+    #Database.update('status','Encrypted','1')
 
-Database.__init__()
+#We define the table we're going to use
+db_name="video_files"
+table_name="movie_files"
+Database.__init__(db_name,table_name)
+
 #print(Database.view())
 #video_track_number()
 working_dir = os.getcwd()
-bin_dir=os.getcwd()+"/bin"
-storage_dir=os.getcwd()+"/storage"
+bin_dir=os.getcwd()+"/../bin"
+storage_dir=os.getcwd()+"/../storage/"
 file="/Users/javierbrines/Documents/Rakuten/video_packaging_platform/BigBuckBunny.mp4"
 output_dir=os.getcwd()+"/output"
-#video_ingestion(file)
+key="F1998AA4629FB47EFFE1A611659BD6A7"
+kid="59C3308F57ADA6AC1D9D34200C484369"
+video_ingestion(file)
 #print(Database.view())
-fragment("file_path",15)
-#encrypt()
-
-
-
-#print(result)
-#        print_output(order)
-
-#def print_output(order):
-#    original = sys.stdout
-#    sys.stdout = open(buffer_file, 'w')
-#    command = os.popen(order)
-#    print(command.read())
-#    print(command.close())
-#    sys.stdout = original
-#    output_file = open(buffer_file, "r")
-#    data = output_file.read()
-
-
-#buffer_file = '/Users/javierbrines/Documents/Rakuten/video_packaging_platform/buffer.txt'
-
-# Store the reference, in case you want to show things again in standard output
-#old_stdout = sys.stdout
-# This variable will store everything that is sent to the standard output
-#result = StringIO()
-#sys.stdout = result
-# Here we can call anything we like, like external modules, and everything that they will send to standard output will be stored on "result"
-#upload()
-# Redirect again the std output to screen
-#sys.stdout = old_stdout
-# Then, get the stdout like a string and process it!
-#result_string = result.getvalue()
-#print(result_string)
-#print("./mp4encrypt --map"+result_string+":prueba:prueba")
-#upload()
-#print(result_string)
+#fragment("origin_file_path",3)
+#encrypt("output_file_path",key,kid,3)
