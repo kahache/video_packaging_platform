097dd86f65058986156a2fe7cfd77e7528ec86c6
Javier Brines <javierbrines@FIGURIN.local>
2020-07-31 12:17:32 +0200

    Main API Flask file

--- app/app.py
+++ app/app.py
@@ -7,28 +7,12 @@ from video_ops import *
 import os
 import subprocess
 import sys
+import requests
 
 #We declare variables
 storage_dir=os.getcwd()+"/../storage/"
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = storage_dir
-
-
-#def movie_files() -> List[Dict]:
-#    config = {
-#        'user': 'root',
-#        'password': 'root',
-#        'host': 'db',
-#        'port': '3306',
-#        'database': 'video_files'
-#    }
-#    connection = mysql.connector.connect(**config)
-#    cursor = connection.cursor()
-#    cursor.execute('SELECT * FROM movie_files')
-#    results=cursor.fetchall()
-#    cursor.close()
-#    connection.close()
-#    return results
 
 #We define the table we're going to use
 db_name="video_files"
@@ -44,13 +28,10 @@ config = {
 Database.__init__(db_name,table_name)
 
 @app.route('/')
-#def index() -> str:
-#    return json.dumps({'movie_files': movie_files()})
 def index():
     result=str(Database.view_all(table_name))
     print(result)
     return result
-
 
 @app.route('/upload_input_content')
 def upload():
@@ -67,15 +48,45 @@ def success():
         path=(os.getcwd()+"/"+f.filename)
         result=Video_ops.video_ingest(path)
         if (result[-1]) == 1:
-            print("HA FUNCIONADO")
             Database.insert_one_row(table_name,"input_content_origin",f.filename)
             input_content_id=(Database.view_one_value("input_content_id",table_name,"input_content_origin",f.filename))
-            print(input_content_id)
             Database.update(table_name,"status","Ingested",str(input_content_id))
+            return render_template("success.html", name = f.filename, input_content_id=input_content_id)
         else:
             print(result)
-        return render_template("success.html", name = f.filename)
+            return "ERROR - Check command line"
 
+@app.route('/packaged_content', methods = ['POST'])
+def package():
+        if request.method == 'POST':
+            print (request.is_json)
+            uploaded_json = request.get_json()
+            input_content_id=uploaded_json['input_content_id']
+            video_key=uploaded_json['key']
+            kid=uploaded_json['kid']
+            #video_fragmentation
+            file_for_fragment=(Database.view_one_value("input_content_origin",table_name,"input_content_id",input_content_id))
+            fragmentation=Video_ops.video_fragment(file_for_fragment)
+            if (fragmentation[-1]) == 1:
+                Database.update(table_name,"status","Fragmented",str(input_content_id))
+                Database.update(table_name,"output_file_path",fragmentation[1],str(input_content_id))
+                Database.update(table_name,"video_key",video_key,str(input_content_id))
+                Database.update(table_name,"kid",kid,str(input_content_id))
+                #Now we encrypt
+                video_track_number=(Database.view_one_value("video_track_number",table_name,"input_content_id",input_content_id))
+                file_to_encrypt=(Database.view_one_value("output_file_path",table_name,"input_content_id",input_content_id))
+                encryptation=Video_ops.video_encrypt(video_track_number,video_key,kid,file_to_encrypt)
+                #updatear BBDD
+            #    print(Database.view_one_value("input_content_id",table_name,"output_file_path",file))
+                #Database.view_all(table_name)
+            else:
+                print(file_for_fragment)
+            #print (input_content_id)
+            #data = json.load(content)
+            #item = data.get('input_content_id')
+            #print(data)
+
+            return 'JSON posted'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
