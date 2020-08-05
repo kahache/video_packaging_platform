7f3211a2f814469652963150c263943655c0e127
Javier Brines <javierbrines@FIGURIN.local>
2020-08-05 15:46:38 +0200

    Small script to quick download files from the list

--- tests/TEST_VIDEOS/download_videos.sh
+++ tests/TEST_VIDEOS/download_videos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #__author__ = "Ka Hache a.k.a. The One & Only Javi"
 #__version__ = "1.0.0"
-#__start_date__ = "10/05/2015"
-#__end_date__ = "14/05/2015"
+#__start_date__ = "05/08/2020"
+#__end_date__ = "05/08/2020"
 #__maintainer__ = "me"
 #__email__ = "little_kh@hotmail.com.com"
 #__status__ = "In production"
