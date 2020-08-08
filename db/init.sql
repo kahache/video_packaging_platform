c89521a43d974074a7e70aaf7528ff38f4dcdeef
Javier Brines <javierbrines@FIGURIN.local>
2020-08-08 12:54:48 +0200

    Main DB declaration for Docker

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,13 @@
-CREATE DATABASE IF NOT EXISTS video_files;
 use video_files;
 
-CREATE TABLE IF NOT EXISTS movie_files (
+CREATE TABLE IF NOT EXISTS uploaded_videos (
+  input_content_id INT PRIMARY KEY AUTO_INCREMENT,
   input_content_origin VARCHAR(255),
-  content_id INT PRIMARY KEY AUTO_INCREMENT,
   video_track_number INT,
-  status VARCHAR(20),
+  status VARCHAR(255),
   output_file_path VARCHAR(255),
-  video_key TEXT,
-  kid TEXT,
+  video_key VARCHAR(255),
+  kid VARCHAR(255),
   packaged_content_id INT UNIQUE,
   url VARCHAR(255)
 );
