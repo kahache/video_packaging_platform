8b4f1fd27eb9f53fb13632cc8eb1ecd20f32f723
Javier Brines <javierbrines@FIGURIN.local>
2020-09-04 18:42:28 +0200

    Main Flask App File

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ __start_date__ = "25th July 2020"
 __end_date__ = "5th August 2020"
 __maintainer__ = "me"
 __email__ = "little_kh@hotmail.com"
-__requirements__ = "SQL-Alchemy, MySQL, Flask-SQLAlchemy, database.py, models.py, video_ops.py, main_ops.py"
+__requirements__ = "SQL-Alchemy, MySQL, Flask-SQLAlchemy, database.py, " \
+                   "models.py, video_ops.py, main_ops.py"
 __status__ = "Production"
 __description__ = """
 This is the main Flask-App for the project. It will start running at
@@ -43,7 +44,10 @@ uploaded_videos = Table('uploaded_videos', metadata, autoload=True)
 @app.route('/')
 def index():
     """Main API documentation as main Webpage"""
-    return redirect("https://app.swaggerhub.com/apis-docs/kahache/VideoPackagingPlatform/1.0.0", code=302)
+    return redirect(
+        "https://app.swaggerhub.com/apis-docs/kahache/VideoPackagingPlatform"
+        "/1.0.0",
+        code=302)
 
 
 @app.route('/upload_input_content')
@@ -57,19 +61,23 @@ def file_received():
     """File processing when video received"""
     file_operation = Main_ops.success()
     if file_operation[0] == 1:
-        return render_template("success.html", name=file_operation[1], input_content_id=file_operation[2])
+        return render_template("success.html", name=file_operation[1],
+                               input_content_id=file_operation[2])
     else:
         return ("ERROR - Check command line")
 
 
 @app.route('/packaged_content', methods=['POST'])
 def background_package():
-    """When called with JSON, it will generate background operations with video files inside internal storage
-    It works with nested operations, if one operation is successful, then starts the next.
+    """When called with JSON, it will generate background operations with
+    video files inside internal storage
+    It works with nested operations, if one operation is successful,
+    then starts the next.
     """
     file_packaging = Main_ops.package()
     if file_packaging[0] == 1:
-        data_set = {"packaged_content_id": [file_packaging[2]], "url": [file_packaging[1]]}
+        data_set = {"packaged_content_id": [file_packaging[2]],
+                    "url": [file_packaging[1]]}
         return (data_set)
     else:
         return ("ERROR - Check command line")
@@ -78,17 +86,21 @@ def background_package():
 @app.route('/packaged_content_id/<int:packaged_content_id>', methods=['GET'])
 def consult_status(packaged_content_id):
     """
-    This method is called for big video files, to check the status of the operations.
-    As the video files' operations update the Database with the status of the file, by knowing that status
+    This method is called for big video files, to check the status of the
+    operations.
+    As the video files' operations update the Database with the status of the
+    file, by knowing that status
     value we can know at which stage point of the process we are
     """
     status_consult = Main_ops.consult_status(packaged_content_id)
     return status_consult
 
+
 @app.route('/videos/')
 def get_videos():
     """
-    This method is to start serving files in the same server. It's just a quick way to access
+    This method is to start serving files in the same server. It's just a
+    quick way to access
     Needs to be called to start running
     Files will appear later on http://0.0.0.0:8080/
     """
@@ -99,4 +111,5 @@ def get_videos():
 
 """ Flask App main launcher """
 if __name__ == '__main__':
+    print("THIS IS THE NEW VERSION", flush=True)
     app.run(host='0.0.0.0')
