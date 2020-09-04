1e3f5d79028f5b10f15117fcc3ee1518393454b9
Javier Brines <javierbrines@FIGURIN.local>
2020-09-04 16:09:05 +0200

    Separate script for DB updating

---
+++ app/update_database.py
@@ -0,0 +1,60 @@
+__author__ = "The One & Only Javi"
+__version__ = "1.0.0"
+__start_date__ = "25th July 2020"
+__end_date__ = "5th August 2020"
+__maintainer__ = "me"
+__email__ = "little_kh@hotmail.com"
+__requirements__ = "SQL-Alchemy, MySQL," \
+                   " Flask-SQLAlchemy, database.py, " \
+                   "models.py, video_ops.py"
+__status__ = "Production"
+__description__ = """
+This is the main background operations script.
+It is meant to be used with app.py, which should call the methods
+"""
+
+import mysql.connector
+import json
+import os
+import subprocess
+import sys
+import requests
+import random
+from flask import *
+from database import *
+from video_ops import *
+from models import *
+from main_ops import *
+from sqlalchemy import *
+from sqlalchemy.sql import *
+from typing import List, Dict
+from datetime import datetime
+
+class Update_DB:
+    def update_after_encrypt(con, input_content_id, output_file_path):
+        output_string = (
+                "\n\n" +
+                datetime.now().strftime("%d/%m/%Y %H:%M:%S") +
+                " - Starting MPEG-DASH transcoding")
+        print(output_string, file=sys.stdout)
+        result = con.execute(
+            uploaded_videos.update().where(
+                uploaded_videos.c.input_content_id
+                == input_content_id).values(
+                status='Encrypted', output_file_path=output_file_path))
+
+    def update_after_dash(con, input_content_id,
+                          dash_output, packaged_content_id):
+        output_string = ("\n\n" + datetime.now().strftime(
+            "%d/%m/%Y %H:%M:%S") +
+                         " - Everything went successful. Returning JSON")
+        print(output_string, file=sys.stdout)
+        result = con.execute(
+            uploaded_videos.update().where(
+                uploaded_videos.c.input_content_id
+                == input_content_id).values(
+                status='Ready', url=dash_output))
+        """We return 1 for OK, url address,
+            and packaged_content_id"""
+        output = (1, dash_output, packaged_content_id)
+        return output
